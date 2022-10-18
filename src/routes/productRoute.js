14586b2f7d5d9ab294b90ebe73070e42a3685a60
Ton Lima <tondrums@hotmail.com>
2022-10-18 20:57:22 -0300

    Criando minha pasta de rotas

---
+++ src/routes/productRoute.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const productController = require('../controllers/productController');
+
+const productRoute = express.Router();
+
+productRoute.get('/', productController.getAll);
+productRoute.get('/:id', productController.getById);
+productRoute.post('/', productController.create);
+
+module.exports = productRoute;
